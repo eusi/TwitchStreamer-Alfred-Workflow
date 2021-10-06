10fa801745d5826fa3e7a822b2e5fdf6f323803a
eusi <eusi.cf@gmail.com>
2021-10-06 09:19:44 +0200

    Update 2.00 - Updated entire Twitch API to latest (incl. new oauth). - Changed livestreamer to streamlink. - Added VLC, MVP and IINA player support. - Updated game covers. - Updated README.md

--- README.md
+++ README.md
@@ -5,19 +5,17 @@ TwitchStreamer Workflow for [Alfred](http://www.alfredapp.com)
 ==============================
 
 ## Summary
-* Search Twitch.tv streams and watch in [VLC](http://www.videolan.org/vlc/index.html) or [mpv](https://mpv.io/) ([Livestreamer](https://github.com/chrippa/livestreamer) required)
+* Search Twitch.tv streams and watch via [VLC](http://www.videolan.org/vlc/index.html) or [mpv](https://mpv.io/) or [IINA](https://iina.io) ([Streamlink (former Livestreamer)](https://streamlink.github.io/index.html) required)
 
-* keywords: `tw`, `twtop`, `twgames`, optional: `twbygame`, `twcover`
+* keywords: `tw`, `twtop`, `twgames`, optional: twcover`
 
-* `twtop`: show top live streams on twitch.tv
+* `twtop`: show top live streams (sorted by number of current viewers)
 
-* `twgames`: show top live games streamed on twitch.tv
+* `twgames`: show top live games streamed (sorted by number of current viewers)
 
-* `tw`: search specific live stream/game on twitch.tv (argument: game, channel or streamer)
+* `tw`: search specific live channel/game
 
-* `twbygame`: search streams by game, result leads to `tw `
-
-* `twcover`: download game icons/covers/posters (optional, you only have to use it once you want to download many new game covers => e.g. because you deleted your outdated cover-folder)
+* `twcover`: download/preload game icons for better user experience
 
 * browse philosophy: `twtop` or `twgames` for getting the most wanted streams on twitch.tv
 
@@ -26,31 +24,31 @@ TwitchStreamer Workflow for [Alfred](http://www.alfredapp.com)
 
 ## Details
 
-Check who is streaming on Twitch.Tv (category gaming) and watch your favorite stream via [Livestreamer](https://github.com/chrippa/livestreamer) on [VLC](http://www.videolan.org/vlc/index.html) or [mpv](https://mpv.io/) (no lags anymore, thanks to buffering).
+Check who is streaming on Twitch.tv (category gaming) and watch your favorite stream via [Streamlink](https://streamlink.github.io/index.html) on [VLC](http://www.videolan.org/vlc/index.html) or [mpv](https://mpv.io/) or [IINA](https://iina.io) (no lags anymore, thanks to buffering).
 
 The main keyword is `tw` and the second word is the game or stream you want to watch (examples: `tw voyboy` or `tw league of legends` or also simply `tw league`). Alternative: Use keyword `twtop` to see the current TOP streams or `twgames` for the current TOP streamed games. Limit of streams is changable via specific workflow ($limit-variable).
 
-With `enter` you can open the stream via [Livestreamer](https://github.com/chrippa/livestreamer) on [VLC](http://www.videolan.org/vlc/index.html) or [mpv](https://mpv.io/). 
+With `enter` you can open the stream via [Streamlink](https://streamlink.github.io/index.html) on [VLC](http://www.videolan.org/vlc/index.html) or [mpv](https://mpv.io/) or [IINA](https://iina.io). 
 
 The streamer list is sorted by number of viewers descending. The quality of the stream is "high". If you want to change it to best (e.g.), feel free to open the existing "Terminal Command" (alfred, workflow-window) and modify the Livestreamer line.
 
-Optional: If you deleted your game icons/covers/posters folder (because many of them are outdated) you should use `twcover` to download all the top game covers. Otherwise `tw ` or `twtop` will do it, but it takes more time and downloads less covers. 
+Optional: If you deleted your game icons/covers folder (because many of them are outdated) you should use `twcover` to download all the top game covers. Otherwise `tw ` or `twtop` will do it, but it takes more time and downloads less covers at the same time. 
 
+## Screenshot: Entry point
+![Workflow Screenshot](https://raw.githubusercontent.com/eusi/TwitchStreamer-Alfred-Workflow/master/screenshots/workflow0.jpg)
 
 ## Screenshot: Browsing TOP STREAMS
 ![Workflow Screenshot](https://raw.githubusercontent.com/eusi/TwitchStreamer-Alfred-Workflow/master/screenshots/workflow1.jpg)
 
 ## Screenshot: Browsing TOP GAMES
-![Workflow Screenshot](https://raw.githubusercontent.com/eusi/TwitchStreamer-Alfred-Workflow/master/screenshots/workflow4.jpg)
-
-## Screenshot: Searching STREAM
-![Workflow Screenshot](https://raw.githubusercontent.com/eusi/TwitchStreamer-Alfred-Workflow/master/screenshots/workflow3.jpg)
-
-## Screenshot: Searching GAME
 ![Workflow Screenshot](https://raw.githubusercontent.com/eusi/TwitchStreamer-Alfred-Workflow/master/screenshots/workflow2.jpg)
 
+## Screenshot: Searching STREAM (Channel or Game)
+![Workflow Screenshot](https://raw.githubusercontent.com/eusi/TwitchStreamer-Alfred-Workflow/master/screenshots/workflow4.jpg)
+
 ## Screenshot: Downloading GAME COVERS/POSTERS
-![Workflow Screenshot](https://raw.githubusercontent.com/eusi/TwitchStreamer-Alfred-Workflow/master/screenshots/workflow5.png)
+![Workflow Screenshot](https://raw.githubusercontent.com/eusi/TwitchStreamer-Alfred-Workflow/master/screenshots/workflow5.jpg)
+![Workflow Screenshot](https://raw.githubusercontent.com/eusi/TwitchStreamer-Alfred-Workflow/master/screenshots/workflow5-.jpg)
 
 ## Screenshot: Watching STREAM
 ![Workflow Screenshot](https://raw.githubusercontent.com/eusi/TwitchStreamer-Alfred-Workflow/master/screenshots/workflow6.jpg)
@@ -58,9 +56,9 @@ Optional: If you deleted your game icons/covers/posters folder (because many of 
 
 ## Download
 
-Required: [Alfred](https://www.alfredapp.com/), [Livestreamer](https://github.com/chrippa/livestreamer) and [VLC](http://www.videolan.org/vlc/index.html) or [mpv](https://mpv.io/)
+Required: [Alfred](https://www.alfredapp.com/), [Streamlink](https://streamlink.github.io/index.html) and [VLC](http://www.videolan.org/vlc/index.html) or [mpv](https://mpv.io/) or [IINA](https://iina.io). 
 
-Made in/with OSX 10.9.1, livestreamer 1.11.1, VLC 2.1.2, PHP 5.3, AppleScript 2.3, Twitch-API/v3
+Made/Tested in/with OSX 10.15.7, Streamlink 2.4.0, VLC 3.0.16, PHP 7.3.11, AppleScript >2.5, Twitch API (Oct. 2021)
 
 Supports [`OneUpdater`](https://github.com/vitorgalvao/alfred-workflows/tree/master/OneUpdater)/AlleyOop/[Monkey Patch](http://www.alfredforum.com/topic/2218-monkey-patch-update-alfred-workflows-via-alleyoop/) (workflow updater).
 
@@ -68,7 +66,14 @@ Supports [`OneUpdater`](https://github.com/vitorgalvao/alfred-workflows/tree/mas
 
 ## Issues
 
-* it takes a lil time between entries because of the twitch.tv-api
+* it takes a lil time between entries because of the Twitch API
+
+
+## Next
+
+* Adding more information for "twgames": number of cannels and viewer (via get streams and game_-_id/user_id)
+
+* Adding more information for "tw *": viewers (via get streams and broadcaster-id)
 
 
 ## Contacts
@@ -85,6 +90,13 @@ GNU General Public License version 3
 
 
 ## Changelog
+
+### 2.00
+
+* Updated entire Twitch API to latest (incl. new oauth).
+* Changed livestreamer to streamlink.
+* Added VLC, MVP and IINA player support.
+* Updated game covers.
 
 ### 1.82
 
--- src/search.php
+++ src/search.php
@@ -1,162 +1,253 @@
 <?php
 	//Twitch API now requires a specific client id per application/developer
-	$clientId = "&client_id=pq1763hfd37807lw0z8pqum3isqr4hd";
+	$clientId = "fzilmsdg3isch2r0zeu1lss52md0xr";
+	$clientSecret = "a9r8vcy9bqp0gxeko6y84vrywuccp5";
+	$twitchAPI = "https://api.twitch.tv/helix";
+	$twitchURL = "https://www.twitch.tv/";
 
-    /**
-     * Add all stream data from the Twitch.tv JSON API.
-     * 
-     * @param int $limit - maximum number of streams sorted by number of viewers descending
-     */
+	$sessionFile = __DIR__ . '/auth.json';
+
+  /**
+   * Initialize TwitchAPI
+   * 
+   */
+	function init() {
+		getAccessToken();
+	}
+
+  /**
+   * Get streams from Twitch.tv sorted by number of viewers descending.
+   * This is used for e.g. twtop
+   * 
+   * @param int $limit - maximum number of results
+   */
 	function getTwitchStreams( $limit ) {
 
-		global $clientId;
-		//https://github.com/justintv/Twitch-API/blob/master/v3_resources/streams.md
-		$url = 'https://api.twitch.tv/kraken/streams?limit=' . $limit . $clientId; 
-		$json = url_get_contents($url);
+		global $twitchURL;
 
-		if (!$json) {
-			return false;
+		$ch = getDataByUrl( "/streams?first=" . $limit );
+		$data = curl_exec( $ch );
+		$info = curl_getinfo( $ch );
+		curl_close( $ch );
+
+		if ($info['http_code'] == 200) {
+		    $data = json_decode($data);
+		    $games = array();
+	
+				if($data != null) {
+					$i = 0;
+					foreach ($data->data as $stream) {
+						$games[$i]["title"] = $stream->title;
+						$games[$i]["streamer"] = $stream->user_name;
+						$games[$i]["viewers"] = $stream->viewer_count;
+						$games[$i]["game"] = $stream->game_name;
+						$games[$i]["url"] = $twitchURL . $stream->user_login;
+						$i++;
+					}
+				}
+				return $games;
 		}
 
-		$data = json_decode($json, false)->streams;
-		$streams = array();
-		
-		if($data != null) {
-			foreach ($data as $key => $stream) {
-				$streams[$key]["title"] = $stream->channel->status;
-				$streams[$key]["streamer"] = $stream->channel->display_name;
-				$streams[$key]["viewers"] = $stream->viewers;
-				$streams[$key]["game"] = $stream->game;
-				$streams[$key]["url"] = $stream->channel->url;
-			}
-		}
-
-		return $streams;
+		echo 'Failed with ' . $info['http_code'];
+		return null;
 	}
 
-    /**
-     * Add all top games data from the Twitch.tv JSON API.
-     * 
-     */
+  /**
+   * Get top games.
+   * This is used for e.g. twgames
+   * 
+   * @param int $limit - maximum number of results
+   */
 	function getTopGames( $limit ) {
 
-		global $clientId;
-		$url = 'https://api.twitch.tv/kraken/games/top?limit=' . $limit . $clientId; 
-		$json = url_get_contents($url);
+		$ch = getDataByUrl( "/games/top?first=" . $limit );
+		$data = curl_exec( $ch );
+		$info = curl_getinfo( $ch );
+		curl_close( $ch );
 
-		if (!$json) {
-			return false;
+		if ($info['http_code'] == 200) {
+		    $data = json_decode($data);
+		    $games = array();
+	
+				if($data != null) {
+					$i = 0;
+					foreach ($data->data as $stream) {
+						$games[$i]["game"] = $stream->name;
+						$games[$i]["top"] = "#".$i;
+						$i++;
+					}
+				}
+				return $games;
 		}
 
-		$data = json_decode($json, false)->top;
-		$games = array();
-		
-		if($data != null) {
-			foreach ($data as $key => $game) {
-				$games[$key]["viewers"] = $game->viewers;
-				$games[$key]["game"] = $game->game->name;
-				$games[$key]["name"] = $game->game->name;
-			}
-		}
-		return $games;
+		echo 'Failed with ' . $info['http_code'];
+		return null;
 	}
 
-    /**
-     * Search streams and add channel stream data from the Twitch.tv JSON API.
-     * 
-     * @param String $q - stream or game name or a part of it
-     * @param int $limit
-     */
+  /**
+   * Search for games and channels.
+   * This is used for e.g. tw *
+   * 
+   * @param String $q - stream or game name or at least a part of it
+   * @param int $limit - maximum number of results
+   */
 	function searchStream( $q, $limit ) {
 
-		global $clientId;
-		//https://github.com/justintv/Twitch-API/blob/master/v3_resources/search.md
-		$url = 'https://api.twitch.tv/kraken/search/streams?q=' . $q . '&limit=' . $limit . $clientId; 
-		$json = url_get_contents($url);
+		global $twitchURL;
 
-		if (!$json) {
-			return false;
-		}
+		$ch = getDataByUrl( "/search/channels?query=" . $q . "&first=" . $limit);
+		$data = curl_exec( $ch );
+		$info = curl_getinfo( $ch );
+		curl_close( $ch );
 
-		$data = json_decode($json, false)->streams;
-		$streams = array();
-		
-		if($data != null) {
-			foreach ($data as $key => $stream) {
-				$streams[$key]["title"] = $stream->channel->status;
-				$streams[$key]["streamer"] = $stream->channel->display_name;
-				$streams[$key]["viewers"] = $stream->viewers;
-				$streams[$key]["game"] = $stream->game;
-				$streams[$key]["url"] = $stream->channel->url;
-			}
-		}
-		return $streams;
-	}
-
-    /**
-     * Search games and add games data from the Twitch.tv JSON API.
-     * 
-     * @param String $q - game name or a part of it
-     * @param int $limit
-     */
-	function searchGame( $q, $limit ) {
-
-		global $clientId;
-		//https://github.com/justintv/Twitch-API/blob/master/v3_resources/search.md
-		$url = 'https://api.twitch.tv/kraken/streams?game=' . $q . '&limit=' . $limit . '&live=true' . $clientId; 
-		$json = url_get_contents($url);
-
-		if (!$json) {
-			return false;
-		}
-
-		$data = json_decode($json, false)->streams;
-		$streams = array();
-		
-		if($data != null) {
-			foreach ($data as $key => $stream) {
-				$streams[$key]["title"] = $stream->channel->status;
-				$streams[$key]["streamer"] = $stream->channel->display_name;
-				$streams[$key]["viewers"] = $stream->viewers;
-				$streams[$key]["game"] = $stream->game;
-				$streams[$key]["url"] = $stream->channel->url;
-			}
-		}
-		return $streams;
-	}
-
-    /**
-     * Search games and return game list.
-     * 
-     */
-	function getTwitchGames() {
-
-		global $clientId;
-		//https://github.com/justintv/Twitch-API/blob/master/v3_resources/games.md
-		$url = 'https://api.twitch.tv/kraken/games/top?limit=100' . $clientId; 
-		$json = url_get_contents($url);
-
-		if (!$json) {
-			return false;
-		}
-
-		$data = json_decode($json, false)->top;
-		$games = array();
-		
-		if($data != null) {
-			foreach ($data as $key => $game) {
-				$games[$key]["name"] = $game->game->name;
-			}
-		}
-		return $games;
-	}
+		if ($info['http_code'] == 200) {
+		    $data = json_decode($data);
+		    $games = array();
 	
-    /**
-     * Functions checks if cover already exists.
-     * 
-     * @param String $game - game name
-     */
-	function checkCover ( $game ) {
+				if($data != null) {
+					$i = 0;
+					foreach ($data->data as $stream) {
+						$games[$i]["title"] = $stream->title;
+						$games[$i]["streamer"] = $stream->display_name;
+						$games[$i]["viewers"] = "";//$stream->viewer_count;
+						$games[$i]["game"] = $stream->game_name;
+						$games[$i]["url"] = $twitchURL . $stream->broadcaster_login;
+						$i++;
+					}
+				}
+				return $games;
+		}
+
+		echo 'Failed with ' . $info['http_code'];
+		return null;
+	}
+
+  /**
+   * Get a list of games sorted by number of viewers descending.
+   * This is used for e.g. twcover
+   * 
+   * @param int $limit - maximum number of results
+   */
+	function getTwitchGames( $limit ) {
+
+		$ch = getDataByUrl( "/games/top?first=" . $limit );
+		$data = curl_exec( $ch );
+		$info = curl_getinfo( $ch );
+		curl_close( $ch );
+
+		if ($info['http_code'] == 200) {
+		    $data = json_decode($data);
+		    $games = array();
+	
+				if($data != null) {
+					$i = 0;
+					foreach ($data->data as $stream) {
+						$games[$i]["name"] = $stream->name;
+						$i++;
+					}
+				}
+				return $games;
+		}
+
+		echo 'Failed with ' . $info['http_code'];
+		return null;
+	}
+
+  /**
+   * Get's the access token from Twitch.tv OAUTH API.
+   * 
+   */
+	function getAccessToken() {
+
+		global $clientId;
+		global $clientSecret;
+		global $twitchAPI;
+		global $sessionFile;
+
+		session_start();
+
+		$json = file_get_contents( $sessionFile );
+		$jsonDecoded = json_decode($json, true);
+		$_SESSION['token'] = $jsonDecoded['access_token'];
+		$_SESSION['expire_time'] = $jsonDecoded['expire_time'];
+
+		// check if token needs to be created
+		if( !isset($_SESSION['token']) || $_SESSION['expire_time'] <= time() ) {
+
+			$_SESSION['token'] = 0;
+
+	    $ch = curl_init('https://id.twitch.tv/oauth2/token');
+	    curl_setopt($ch, CURLOPT_POST, true);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+	        'client_id' => $clientId,
+	        'client_secret' => $clientSecret,
+	        'grant_type' => 'client_credentials'
+	    ));
+
+	    // fetch the data
+	    $r = curl_exec($ch);
+	    $i = curl_getinfo($ch);
+	    // close the request
+	    curl_close($ch);
+
+	    if ($i['http_code'] == 200) {
+	        $token = json_decode($r);
+	        $token->expire_time = $token->expires_in + time();
+
+	        if (json_last_error() == JSON_ERROR_NONE) {
+	            $_SESSION['token'] = $token->access_token;
+	            
+	            file_put_contents( $sessionFile, json_encode($token), JSON_PRETTY_PRINT );
+	            exit;
+	        } else {
+	            echo 'Failed to parse the JSON at code for token exchange';
+	        }
+	    } else {
+	        echo 'Error for token exchange: ' . print_r($r, true);
+	    }
+		}
+	}
+
+  /**
+   * Get stream data by url with available token.
+   * 
+   * @param String $url - url of Twitch API, e.g. "/streams/"
+   */
+	function getDataByUrl( $url ) {
+		global $clientId;
+		global $twitchAPI;
+		global $sessionFile;
+
+		session_start();
+
+		$json = file_get_contents( $sessionFile );
+		$jsonDecoded = json_decode($json, true);
+		$_SESSION['token'] = $jsonDecoded['access_token'];
+		$_SESSION['expire_time'] = $jsonDecoded['expire_time'];
+
+		$ch = curl_init();
+		$UURL = $twitchAPI . $url;
+		curl_setopt($ch, CURLOPT_URL, $UURL);
+		//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		    'Authorization: Bearer '.$_SESSION['token'],
+		    'Client-Id: '.$clientId
+		));
+
+		return $ch;
+	}
+
+  /**
+   * Functions checks if cover/icon already exists.
+   * 
+   * @param String $game - game name
+   */
+	function checkCover( $game ) {
 
 		$game = rawurlencode( $game );
 		$file = 'images/'.$game.'.jpg';
@@ -172,12 +263,12 @@
 		}
 	}
 
-    /**
-     * Functions checks if cover already exists (twcover only).
-     * 
-     * @param String $game - game name
-     */
-	function checkingCovers ( $game ) {
+  /**
+   * Functions checks if cover already exists (twcover only).
+   * 
+   * @param String $game - game name
+   */
+	function checkingCovers( $game ) {
 
 		$game = rawurlencode( $game );
 		$file = 'images/'.$game.'.jpg';
@@ -187,30 +278,21 @@
 		}
 	}
 
-    /**
-     * Functions downloads cover from Twitch.tv.
-     * 
-     * @param String $game - game name
-     */
-	function downloadCover ( $game ) {
+  /**
+   * Functions downloads cover from Twitch.tv.
+   * 
+   * @param String $game - game name
+   */
+	function downloadCover( $game ) {
 
 		$ok = false;
-		$url = "http://static-cdn.jtvnw.net/ttv-boxart/" . $game . "-92x128.jpg";
+		$url = "https://static-cdn.jtvnw.net/ttv-boxart/" . $game . "-92x128.jpg";
 		
 		try 
 		{
 			if( urlExists( $url ) ) {
 
-				/*$arrContextOptions=array(
-				    "ssl"=>array(
-				        "cafile" => "/bundle/cacert.pem", //https://curl.haxx.se/docs/caextract.html
-				        "verify_peer"=> true,
-				        "verify_peer_name"=> true,
-				    ),
-				);*/
-
 				$temp = "images/". $game .".jpg";
-				//file_put_contents( $temp, fopen( $url , 'r'));
 
 				//using curl instead now
 				$ch = curl_init($url);
@@ -228,15 +310,15 @@
 		{	
 			echo "Error: " . $e->getMessage();
 		}
-
+		
 	 	return $ok;
 	}
 
-    /**
-     * Functions checks if url exists.
-     * 
-     * @param String $game - game name
-     */
+  /**
+   * Functions checks if url exists.
+   * 
+   * @param String $game - game name
+   */
 	function urlExists($file) {
 
 		$file_headers = @get_headers($file);
@@ -248,39 +330,36 @@
 		}
 	}
 
-    /**
-     * Functions replaces file_get_contents (get content of url).
-     * 
-     * @param String $url
-     */
-	function url_get_contents ($url) {
+  /**
+   * Functions replaces file_get_contents (get content of url).
+   * 
+   * @param String $url
+   */
+	function url_get_contents($url) {
 
-	    if (function_exists('curl_exec'))
-	    { 
-	        $conn = curl_init($url);
-            //curl_setopt($ch, CURLOPT_HEADER, false);
-	        //curl_setopt($conn, CURLOPT_SSLVERSION,3); 
-	        //curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, true);
-	        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, true);
-	        curl_setopt($conn, CURLOPT_FRESH_CONNECT,  true);
-	        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
-	        $url_get_contents_data = (curl_exec($conn));
-	        curl_close($conn);
-	    }
-	    elseif(function_exists('file_get_contents'))
-	    {
-	        $url_get_contents_data = file_get_contents($url);
-	    }
-	    elseif(function_exists('fopen') && function_exists('stream_get_contents'))
-	    {
-	        $handle = fopen ($url, "r");
-	        $url_get_contents_data = stream_get_contents($handle);
-	    }
-	    else
-	    {
-	        $url_get_contents_data = false;
-	    }
-		
+    if (function_exists('curl_exec'))
+    { 
+        $conn = curl_init($url);
+        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($conn, CURLOPT_FRESH_CONNECT,  true);
+        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+        $url_get_contents_data = (curl_exec($conn));
+        curl_close($conn);
+    }
+    elseif(function_exists('file_get_contents'))
+    {
+        $url_get_contents_data = file_get_contents($url);
+    }
+    elseif(function_exists('fopen') && function_exists('stream_get_contents'))
+    {
+        $handle = fopen ($url, "r");
+        $url_get_contents_data = stream_get_contents($handle);
+    }
+    else
+    {
+        $url_get_contents_data = false;
+    }
+	
 		return $url_get_contents_data;
 	} 
 
